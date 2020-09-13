f8b228dc5f008ec13b6936bdf16ccf25340650ea
Kevin W. van Rooijen <kevin.van.rooijen@gmail.com>
2020-09-13 21:32:29 +0200

    Return database

--- src/duct/sql/gungnir.clj
+++ src/duct/sql/gungnir.clj
@@ -5,10 +5,12 @@
    [integrant.core :as ig]))
 
 (defmethod ig/init-key ::make-datasource! [_ opts]
-  (gungnir.database/make-datasource! opts))
+  (gungnir.database/make-datasource! opts)
+  gungnir.database/*database*)
 
 (defmethod ig/resolve-key ::make-datasource! [_ _opts]
   (sql/->Boundary {:datasource gungnir.database/*database*}))
 
 (defmethod ig/init-key ::set-datasource! [_ opts]
-  (gungnir.database/set-datasource! opts))
+  (gungnir.database/set-datasource! opts)
+  gungnir.database/*database*)
