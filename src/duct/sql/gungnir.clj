c02c02b6d2b1552653e8a2aa1966def28792a23b
Kevin W. van Rooijen <kevin.van.rooijen@gmail.com>
2021-01-20 00:31:46 +0100

    Use gungnir.database/*datasource* instead of gungnir.database/*database*

--- src/duct/sql/gungnir.clj
+++ src/duct/sql/gungnir.clj
@@ -6,11 +6,11 @@
 
 (defmethod ig/init-key ::make-datasource! [_ opts]
   (gungnir.database/make-datasource! opts)
-  gungnir.database/*database*)
+  gungnir.database/*datasource*)
 
 (defmethod ig/resolve-key ::make-datasource! [_ _opts]
-  (sql/->Boundary {:datasource gungnir.database/*database*}))
+  (sql/->Boundary {:datasource gungnir.database/*datasource*}))
 
 (defmethod ig/init-key ::set-datasource! [_ opts]
   (gungnir.database/set-datasource! opts)
-  gungnir.database/*database*)
+  gungnir.database/*datasource*)
